f419327f147d65d69229977754f9a3a5be053d13
Malik Kernel <dm372842@gmail.com>
2026-02-03 20:58:23 +0300

    ...

--- icartable_docs/settings.py
+++ icartable_docs/settings.py
@@ -22,7 +22,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wg^jh+bphr@!+_u(#bm_f_ck_b^(1xbm_t=58jcuqb947)io(='
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
